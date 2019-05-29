d0125e2cf6a3ebd9d5fd53384ec410d3a8ea2e40
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-05-29 20:27:54 +0900

    userテーブルストロングパラメータ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :user_params, if: :devise_controller?
 
   def user_params 
-    devise_parameter_sanitizer.permit(:sgin_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
