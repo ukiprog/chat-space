75dd152f853cf2820d51201f5fc4b4b5c2e67494
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-05-29 20:04:58 +0900

    devise install
    devise install
    userテーブルの作成
    viewファイルの準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def user_params 
+    devise_parameter_sanitizer.permit(:sgin_up, keys: [:name])
+  end
 end
