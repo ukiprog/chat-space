d370134efc436e3a698936695a6228d65734e5e3
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-06-01 22:28:10 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   patch '/users/update' => 'users#update'
   resources :groups, only: [:new, :create, :edit, :update]
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
