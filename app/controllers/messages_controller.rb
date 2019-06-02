8c558d4a6ab802505949a68fcab3462bbd0ed5da
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-06-02 15:42:36 +0900

    message create実装（動作未確認）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,29 @@
 class MessagesController < ApplicationController
+  before_action :get_group
+
   def index
     @groups = current_user.groups
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+  end
+
+  def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group)
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def get_group
+    @group = Group.find(params[:group_id])
   end
 end
