6f26b8ae8c3930ac7614dd33280ea99920783052
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-06-06 20:54:20 +0900

    factories/user　修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     password = Faker::Internet.password(8)
     name {Faker::Name.name}
-    email {Faker::Internet.free_email}
     password {password}
     password_confirmation {password}
     sequence(:email) {Faker::Internet.email}
