28a74a0237797bb98e7761ed75e0abaaed2d809a
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-06-06 21:16:54 +0900

    devise rspecの準備

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require_relative 'support/controller_macros' # or require_relative './controller_macros' if write in `spec/support/devise.rb`
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -59,4 +61,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include ControllerMacros, :type => :controller
 end
