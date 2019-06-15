4f655999aed4b566ad49148a2f87d10f32099a57
ukiprog <50447794+ukiprog@users.noreply.github.com>
2019-06-15 16:11:45 +0900

    グループ作成者がグループのメンバーとして登録されない不具合を修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,13 +6,17 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
   end
 
   def create
     @group = Group.new(group_params)
-    @group.save
-    redirect_to root_path, notice: 'グループを作成しました'
+    @group.users << current_user
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      flash.now[:alert] = 'そのグループ名はすでに使われています'
+      render :new
+    end
   end
 
   def edit
